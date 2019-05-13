895e0a9c07d066f1f1d8de416b97e9eec5fa1fcb
orikarudo <okaccounts@gmail.com>
2019-05-13 00:07:28 -0400

    first commit

---
+++ Project 6/Project 6Tests/Project_6Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Project_6Tests.swift
+//  Project 6Tests
+//
+//  Created by Ori Karudo on 5/12/19.
+//  Copyright © 2019 Ori Karudo. All rights reserved.
+//
+
+import XCTest
+@testable import Project_6
+
+class Project_6Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
